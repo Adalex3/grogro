1871f4df870d3c4be0e132488d3db18355d00ff6
Adalex3 <adalex3@icloud.com>
2024-10-05 03:28:42 -0400

    Reorganized

--- mysite/flask_app.py
+++ mysite/flask_app.py
@@ -11,8 +11,12 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/upload-receipt', methods=['POST'])
-def upload_receipt():
+@app.route('/receipt_scan')
+def receipt_scan():
+    return render_template('receipt_scan.html')
+
+@app.route('/process-receipt', methods=['POST'])
+def process_receipt():
     # Get base64-encoded image data from form
     image_data = request.form['imageData']
     image_data = image_data.replace('data:image/png;base64,', '')
@@ -21,13 +25,13 @@ def upload_receipt():
     image = Image.open(BytesIO(base64.b64decode(image_data)))
 
     # Save the image temporarily for display in show_photo.html
-    image_path = 'static/uploaded_image.png'
+    image_path = 'static/receipt.png'
     image.save(image_path)
 
     ocr_text = pytesseract.image_to_string(image)
 
     # Render show_photo.html with OCR text and image
-    return render_template('show_photo.html', filename='uploaded_image.png', ocr_text=ocr_text)
+    return render_template('show_photo.html', filename='receipt.png', ocr_text=ocr_text)
 
 if __name__ == '__main__':
     app.run(debug=True,port=5001)
